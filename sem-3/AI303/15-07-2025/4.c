8f5e7017dcb811d889823f5f1988343ca00c6f18
Hardik Kumar Sinha <hardikkumarsinha@gmail.com>
2025-07-24 22:08:10 +0530

    koustubh ;)

--- sem-3/AI303/15-07-2025/4.c
+++ sem-3/AI303/15-07-2025/4.c
@@ -1,1 +1,18 @@
-same as 1.c
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {0,8,3,56,2};
+    int a = 0, b = 0;
+
+    for (int i = 0; i < 5; i++){
+        if (a < arr[i]){
+            b = a;
+            a = arr[i];
+        }
+    }
+
+    printf("\n%d is the largest number\n", a);
+    printf("%d is the second largest number", b);
+
+    return 0;
+}
